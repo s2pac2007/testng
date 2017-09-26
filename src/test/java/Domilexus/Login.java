4645c295ba7e0486e22d8f647a800262329542b6
stupak2007 <stupak2007@gmail.com>
2017-09-26 13:35:00 +0300

    Got files from repo testNG. Made some amending with class Login.java
    Need to findout solution for running tests on eclipse. Fixed errors
    related with webdriver etc.

--- src/test/java/Domilexus/Login.java
+++ src/test/java/Domilexus/Login.java
@@ -7,6 +7,7 @@ import org.testng.annotations.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -14,8 +15,13 @@ public class Login extends Domilexus.TestBase {
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
+	WebDriver driver = new ChromeDriver();
   @Test
   public void testLogin() throws Exception {
+	 
+	System.setProperty("webdriver.chrome.driver", "D:\\DEV\\chromedriver.exe");
+
+	String baseUrl = "https://localhost";
 	driver.get(baseUrl + "/php4dvd/");
     driver.findElement(By.id("username")).clear();
     driver.findElement(By.id("username")).sendKeys("admin");
@@ -25,6 +31,7 @@ public class Login extends Domilexus.TestBase {
   }
 
   private boolean isElementPresent(By by) {
+
     try {
       driver.findElement(by);
       return true;
@@ -34,6 +41,7 @@ public class Login extends Domilexus.TestBase {
   }
 
   private String closeAlertAndGetItsText() {
+
     try {
       Alert alert = driver.switchTo().alert();
       String alertText = alert.getText();
